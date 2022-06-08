200848b09a17bcc57bd10617095db3042750dad2
Sangwoo Han <sangwoo.h@algorima.io>
2022-06-09 00:16:47 +0900

    Update gen_top1000.sh

--- scripts/gen_top1000.sh
+++ scripts/gen_top1000.sh
@@ -8,3 +8,9 @@ for i in $(seq 0 23); do
     --output data/top1000/train_top1000_$(printf %02d $i).txt \
     --bm25 --language ko --hits 1000 --batch-size 256 --threads 32
 done
+
+python -m pyserini.search.lucene \
+    --index data/test.index \
+    --topics data/top1000/test_queries_00.tsv \
+    --output data/top1000/test_top1000_00.txt \
+    --bm25 --language ko --hits 1000 --batch-size 256 --threads 32
