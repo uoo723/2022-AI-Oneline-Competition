7abd8a0e2ae900ea2a3cc84d1ae5b0524af6544f
Sangwoo Han <sangwoo.h@algorima.io>
2022-06-20 15:33:07 +0900

    Update run_colbert.sh

--- scripts/run_colbert.sh
+++ scripts/run_colbert.sh
@@ -11,39 +11,55 @@ args=(
     --model-name $MODEL
     --dataset-name $DATASET
     --run-script $0
-    --pretrained-model-name "monologg/koelectra-small-v3-discriminator"
-    # --pretrained-model-name "monologg/koelectra-base-v3-discriminator"
-    # --pretrained-model-name "gogamza/kobart-base-v2"
+    --pretrained-model-name "hyunwoongko/kobart"
+    --skip-test
+    --log-run-id
     --n-feature-layers 5
+    --topk-candidates 100
     --optim-name "adamw"
-    --lr 1e-4
-    --num-epochs 10
-    --train-batch-size 16
+    --lr 2.5e-5
+    --num-epochs 5
+    --train-batch-size 4
     --test-batch-size 32
-    --accumulation-step 2
+    --accumulation-step 8
     --early-criterion 'mrr'
     --seed $1
     --swa-warmup 1
-    --eval-step 1000
+    --eval-step 5000
     --early 5
     --mp-enabled
     --gradient-max-norm 5.0
     --num-workers 8
     --experiment-name "ColBERT"
-    # --run-id "efe8a17d663645389ff4b92f9f82da44"
-    # --reset-early
-    --query-max-length 60
+    --query-max-length 80
     --passage-max-length 512
     --valid-size 500
     --num-pos 1
     --num-neg 1
-    --topk-candidates 50
     --shard-idx 0
     --shard-idx 1
     --shard-idx 2
     --shard-idx 3
     --shard-idx 4
     --shard-idx 5
+    --shard-idx 6
+    --shard-idx 7
+    --shard-idx 8
+    --shard-idx 9
+    --shard-idx 10
+    --shard-idx 11
+    --shard-idx 12
+    --shard-idx 13
+    --shard-idx 14
+    --shard-idx 15
+    --shard-idx 16
+    --shard-idx 17
+    --shard-idx 18
+    --shard-idx 19
+    --shard-idx 20
+    --shard-idx 21
+    --shard-idx 22
+    --shard-idx 23
 )
 
 python main.py train-colbert "${args[@]}"
