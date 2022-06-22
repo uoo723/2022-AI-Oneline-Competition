1c287bc0b8b99309960030ba1ecf052044ee0bac
Sangwoo Han <sangwoo.h@algorima.io>
2022-06-22 09:53:36 +0900

    Update prediction scripts

--- scripts/run_prediction.sh
+++ scripts/run_prediction.sh
@@ -10,7 +10,7 @@ MONOBERT_RUN_ID=${MONOBERT_RUN_ID:-$(cat $TMP_DIR/monoBERT_run_id)}
 TOPK=${TOPK:-500}
 TOPK2=${TOPK2:-20}
 
-export SUBMISSION_FILE=${SUBMISSION_FILE:-submission24.csv}
+export SUBMISSION_FILE=${SUBMISSION_FILE:-submission.csv}
 export TOPK_FILE=top${TOPK}_colbert_${COLBERT_RUN_ID}.csv
 
 RUN_ID=$COLBERT_RUN_ID TOPK=$TOPK ./scripts/run_colbert_prediction.sh
